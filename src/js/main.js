436cd027edf55ca6025dc7f87816fe219f7e2896
Alina <sijunma116@gmail.com>
2021-07-26 18:22:07 +0300

    product section

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,11 @@ $('.header-slider').slick({
     dots: true,
     arrows: false,
 });
+
+$('.product-main__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+});
