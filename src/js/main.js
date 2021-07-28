47db2593b0c0aae28466912a6bfe4980a442f612
Alina <sijunma116@gmail.com>
2021-07-28 14:46:08 +0300

    Adaptive of main page

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,20 @@ $('.product-main__slider').slick({
     dots: true,
     arrows: false,
 });
+
+$('.js-navbar__burger-btn').click(evt => {
+    evt.preventDefault();
+    $('.js-navbar__primary-menu').fadeToggle();
+});
+
+let toggle = $(window).width() < 576;
+$(window).on('resize', function (event) {
+
+    if ($(window).width() >= 576 && toggle) {
+        $('.js-navbar__primary-menu').hide().fadeIn(1000);
+        toggle = false;
+    } else if ($(window).width() < 576 && !toggle) {
+        $('.js-navbar__primary-menu').hide().fadeOut(1000);
+        toggle = true;
+    }
+});
